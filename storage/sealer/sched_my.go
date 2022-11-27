c7e92477a46f944ceb9eccfc78294b080cd4d16a
sedasdas <qq1820029262@gmail.com>
2022-11-27 23:23:50 +0800

    add

--- storage/sealer/sched_my.go
+++ storage/sealer/sched_my.go
@@ -4,7 +4,7 @@ type local struct {
 	max int
 }
 
-func SchedLocal(l local, task *WorkerRequest, request *SchedWindowRequest, worker *WorkerHandle) bool {
+func SchedLocal(task *WorkerRequest, request *SchedWindowRequest, worker *WorkerHandle) bool {
 	log.Debugf(worker.Info.Hostname)
 	log.Debugf(task.Sector.ID.Number.String())
 	if worker.Info.Hostname == "hcxj-10-0-5-71" {
