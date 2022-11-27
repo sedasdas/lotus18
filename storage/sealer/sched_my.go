ff38ea58a0d055003c1bfb1db866aeb962391001
sedasdas <qq1820029262@gmail.com>
2022-11-27 23:49:07 +0800

    add

--- storage/sealer/sched_my.go
+++ storage/sealer/sched_my.go
@@ -1,16 +1,28 @@
 package sealer
 
 type local struct {
-	max int
+	sectors []string
 }
+
+var sectors []string
 
 func SchedLocal(task *WorkerRequest, request *SchedWindowRequest, worker *WorkerHandle) bool {
 	log.Debugf(worker.Info.Hostname)
 	log.Debugf(task.Sector.ID.Number.String())
+	//per(sector task.Sector.ID.Number.String())
+
 	if worker.Info.Hostname == "hcxj-10-0-5-71" {
 		log.Debugf("RIGHT??????????????????????????????")
+		sectors = append(sectors, task.Sector.ID.Number.String())
+		log.Debugf("len=%d cap=%d slice=%v\n", len(sectors), cap(sectors), sectors)
 		return true
 	}
 
 	return false
+}
+func read(l local) {
+
+}
+func per(sector string, l local) {
+
 }
