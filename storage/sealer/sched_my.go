3cbeb4409f1c106df20c698b88e665cd5c6f5356
sedasdas <qq1820029262@gmail.com>
2022-11-29 22:14:18 +0800

    add

--- storage/sealer/sched_my.go
+++ storage/sealer/sched_my.go
@@ -1,6 +1,9 @@
 package sealer
 
-import "github.com/filecoin-project/lotus/storage/sealer/storiface"
+import (
+	"fmt"
+	"github.com/filecoin-project/lotus/storage/sealer/storiface"
+)
 
 type Local struct {
 	ServerName string   `json:"serverName"`
@@ -18,25 +21,24 @@ func SchedLocal(task *WorkerRequest, request *SchedWindowRequest, worker *Worker
 	//per(task.TaskType.Short(), task.Sector.ID.Number.String(), worker.Info.Hostname)
 	//log.Debugf(task.Sector.ID.Number.String())
 	//per(sector task.Sector.ID.Number.String())
+
+	wrpc, _ := worker.workerRpc.TaskTypes(task.Ctx)
+	for taskType, s := range wrpc {
+		fmt.Println("rpc"+taskType.Short(), s)
+	}
 	for id, handle := range workers {
 		id.String()
 		log.Debugf(handle.Info.Hostname)
 	}
 	for taskType, i := range worker.active.taskCounters {
-		log.Debugf(taskType.WorkerType(), i)
+		log.Debugf("taskCounters"+taskType.WorkerType(), i)
 	}
 	log.Debugf("task is ----------------------" + task.TaskType.Short())
 	if worker.Info.Hostname == "hcxj-10-0-1-185" {
 		ch <- task.Sector.ID.Number.String()
 		log.Debugf("RIGHT??????????????????????????????")
 		//sectors = append(sectors, task.Sector.ID.Number.String())
-		for _, sector := range sectors {
-			log.Debugf(sector)
-			if task.Sector.ID.Number.String() == sector {
-				log.Debugf("分配了" + sector)
-				return true
-			}
-		}
+		return true
 		//log.Debugf("len=%d cap=%d slice=%v\n", len(sectors), cap(sectors), sectors)
 	}
 
