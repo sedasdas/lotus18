210d2e006fe6d086a6c6ba5555d29e6a4ba73bcc
sedasdas <qq1820029262@gmail.com>
2022-11-30 00:37:44 +0800

    add

--- storage/sealer/sched_my.go
+++ storage/sealer/sched_my.go
@@ -1,7 +1,6 @@
 package sealer
 
 import (
-	"fmt"
 	"github.com/filecoin-project/lotus/storage/sealer/storiface"
 )
 
@@ -24,34 +23,37 @@ func SchedLocal(task *WorkerRequest, request *SchedWindowRequest, worker *Worker
 	//log.Debugf(task.Sector.ID.Number.String())
 	//per(sector task.Sector.ID.Number.String())
 
-	wrpc, _ := worker.workerRpc.TaskTypes(task.Ctx)
-	for taskType, s := range wrpc {
-		fmt.Println("rpc"+taskType.Short(), s)
-	}
-	for id, handle := range workers {
-		id.String()
-		log.Debugf(handle.Info.Hostname)
-	}
+	//wrpc, _ := worker.workerRpc.TaskTypes(task.Ctx)
+	//for taskType, s := range wrpc {
+	//	fmt.Println("rpc"+taskType.Short(), s)
+	//}
+	//for id, handle := range workers {
+	//	id.String()
+	//	log.Debugf(handle.Info.Hostname)
+	//}
 	//for taskType, i := range worker.active.taskCounters {
 	log.Debugf(string(len(se)))
 	//log.Debugf("taskCounters"+taskType.Short(), i)
 	log.Debugf("task is ----------------------" + task.TaskType.Short())
 	if task.TaskType.Short() == "FIN" && len(se) > 0 {
 		delete(se, task.TaskType.Short())
+		log.Debugf("拉取文件中")
 		return true
 		//se[task.Sector.ID.Number.String()] = worker.Info.Hostname
 	}
 
 	_, ok := se[task.Sector.ID.Number.String()]
 	if ok {
+		log.Debugf(worker.Info.Hostname + "正在执行" + task.TaskType.Short())
 		return true
 	}
 	if task.TaskType.Short() == "AP" {
 		se[task.Sector.ID.Number.String()] = worker.Info.Hostname
+		log.Debugf("分配了AP" + task.Sector.ID.Number.String() + "给" + worker.Info.Hostname)
 		return true
 	}
 
-	log.Debugf("task is ----------------------" + task.TaskType.Short())
+	//log.Debugf("task is ----------------------" + task.TaskType.Short())
 	//if worker.Info.Hostname == "hcxj-10-0-1-185" {
 	//ch <- task.Sector.ID.Number.String()
 	//log.Debugf("RIGHT??????????????????????????????")
