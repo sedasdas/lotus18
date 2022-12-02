1eba61d3a6746766391b10c03817dada9e4a33af
sedasdas <qq1820029262@gmail.com>
2022-12-02 21:08:43 +0800

    add

--- storage/sealer/sched_my.go
+++ storage/sealer/sched_my.go
@@ -82,7 +82,11 @@ func SchedLocal(task *WorkerRequest, request *SchedWindowRequest, worker *Worker
 	*/
 	h, ok := scene.Load(task.Sector.ID.Number.String())
 	if ok {
-
+		if task.TaskType.Short() == "GET" && h == worker.Info.Hostname {
+			scene.Delete(task.Sector.ID.Number.String())
+			log.Debugf("拉取文件中")
+			return true
+		}
 		if h == worker.Info.Hostname {
 
 			log.Debugf(worker.Info.Hostname + "正在执行" + task.Sector.ID.Number.String() + "----" + task.TaskType.Short())
