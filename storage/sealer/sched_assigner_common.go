aac46379d7da78201240913ece7440edc8a41a85
sedasdas <qq1820029262@gmail.com>
2022-12-02 22:10:25 +0800

    add

--- storage/sealer/sched_assigner_common.go
+++ storage/sealer/sched_assigner_common.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/filecoin-project/lotus/storage/sealer/sealtasks"
 	"os"
 	"sync"
 )
@@ -104,7 +105,7 @@ func (a *AssignerCommon) TrySched(sh *Scheduler) {
 				//}
 				//SchedLocal(task,windowRequest)
 
-				if !SchedLocal(task, windowRequest, worker) {
+				if task.TaskType != sealtasks.TTFetch && !SchedLocal(task, windowRequest, worker) {
 					continue
 				}
 				//needRes := worker.Info.Resources.ResourceSpec(task.Sector.ProofType, task.TaskType)
