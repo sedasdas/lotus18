b2fb69f764df40167f567d397392e99ef9c5401b
sedasdas <qq1820029262@gmail.com>
2023-01-22 15:31:10 +0800

    恢复原版

--- storage/sealer/sched_assigner_common.go
+++ storage/sealer/sched_assigner_common.go
@@ -7,41 +7,56 @@ import (
 )
 
 type WindowSelector func(sh *Scheduler, queueLen int, acceptableWindows [][]int, windows []SchedWindow) int
+
+// AssignerCommon is a task assigner with customizable parts
 type AssignerCommon struct {
-	taskChannel chan *Scheduler
-	wg          sync.WaitGroup
+	WindowSel WindowSelector
 }
 
-func NewAssignerCommon() *AssignerCommon {
-	a := &AssignerCommon{
-		taskChannel: make(chan *Scheduler),
-	}
-	return a
-}
+var _ Assigner = &AssignerCommon{}
 
 func (a *AssignerCommon) TrySched(sh *Scheduler) {
 	windowsLen := len(sh.OpenWindows)
 	queueLen := sh.SchedQueue.Len()
+
 	log.Debugf("SCHED %d queued; %d open windows", queueLen, windowsLen)
+
 	if windowsLen == 0 || queueLen == 0 {
 		// nothing to schedule on
 		return
 	}
 
-	acceptableWindowsPool := &sync.Pool{New: func() interface{} { return make([]int, 0, windowsLen) }}
+	windows := make([]SchedWindow, windowsLen)
+	for i := range windows {
+		windows[i].Allocated = *NewActiveResources()
+	}
+	acceptableWindows := make([][]int, queueLen) // QueueIndex -> []OpenWindowIndex
 
-	a.wg.Add(queueLen)
+	// Step 1
+	throttle := make(chan struct{}, windowsLen)
+
+	var wg sync.WaitGroup
+	wg.Add(queueLen)
 	for i := 0; i < queueLen; i++ {
-		go func(i int) {
-			defer a.wg.Done()
-			task := (*sh.SchedQueue)[i]
-			task.IndexHeap = i
-			acceptableWindows := acceptableWindowsPool.Get().([]int)[:0]
+		throttle <- struct{}{}
+
+		go func(sqi int) {
+			defer wg.Done()
+			defer func() {
+				<-throttle
+			}()
+
+			task := (*sh.SchedQueue)[sqi]
+			task.IndexHeap = sqi
+
 			var havePreferred bool
+
 			for wnd, windowRequest := range sh.OpenWindows {
+
 				worker, ok := sh.Workers[windowRequest.Worker]
 				if !ok {
 					log.Errorf("worker referenced by windowRequest not found (worker: %s)", windowRequest.Worker)
+					// TODO: How to move forward here?
 					continue
 				}
 
@@ -53,6 +68,9 @@ func (a *AssignerCommon) TrySched(sh *Scheduler) {
 				if task.TaskType != sealtasks.TTFetch && !SchedMyn(task, worker) {
 					continue
 				}
+
+				// TODO: allow bigger windows
+
 				rpcCtx, cancel := context.WithTimeout(task.Ctx, SelectorTimeout)
 
 				ok, preferred, err := task.Sel.Ok(rpcCtx, task.TaskType, task.Sector.ProofType, worker)
@@ -73,19 +91,63 @@ func (a *AssignerCommon) TrySched(sh *Scheduler) {
 
 				if preferred && !havePreferred {
 					// all workers we considered previously are much worse choice
-					acceptableWindows = acceptableWindows[:0]
+					acceptableWindows[sqi] = acceptableWindows[sqi][:0]
 					havePreferred = true
 				}
 
-				acceptableWindows = append(acceptableWindows, wnd)
+				acceptableWindows[sqi] = append(acceptableWindows[sqi], wnd)
 			}
 
-			if len(acceptableWindows) == 0 {
+			if len(acceptableWindows[sqi]) == 0 {
 				return
 			}
-			acceptableWindowsPool.Put(acceptableWindows)
+
+			// Pick best worker (shuffle in case some workers are equally as good)
+
 		}(i)
 	}
-	a.wg.Wait()
+
+	wg.Wait()
+	log.Debugf("SCHED windows: %+v", windows)
+	log.Debugf("SCHED Acceptable win: %+v", acceptableWindows)
+
+	// Step 2
+	scheduled := a.WindowSel(sh, queueLen, acceptableWindows, windows)
+
+	// Step 3
+
+	if scheduled == 0 {
+		return
+	}
+
+	scheduledWindows := map[int]struct{}{}
+	for wnd, window := range windows {
+		if len(window.Todo) == 0 {
+			// Nothing scheduled here, keep the window open
+			continue
+		}
+
+		scheduledWindows[wnd] = struct{}{}
+
+		window := window // copy
+		select {
+		case sh.OpenWindows[wnd].Done <- &window:
+		default:
+			log.Error("expected sh.OpenWindows[wnd].Done to be buffered")
+		}
+	}
+
+	// Rewrite sh.OpenWindows array, removing scheduled windows
+	newOpenWindows := make([]*SchedWindowRequest, 0, windowsLen-len(scheduledWindows))
+	for wnd, window := range sh.OpenWindows {
+		if _, scheduled := scheduledWindows[wnd]; scheduled {
+			// keep unscheduled windows open
+			continue
+		}
+
+		newOpenWindows = append(newOpenWindows, window)
+	}
+
+	sh.OpenWindows = newOpenWindows
 
 }
